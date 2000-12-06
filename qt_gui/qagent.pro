3474846f35064ed881fc2a75595f80201d0cb129
mordred <mordred@928a95fc-701e-0410-818f-83ad171ca9e6>
2000-12-06 23:12:46 +0000

    Internationalization support again; cleaned up changelog.

---
+++ qt_gui/qagent.pro
@@ -0,0 +1,3 @@
+HEADERS			= qagentimpl.h qagent.h qt_gui.h
+SOURCES			= qagentimpl.cpp qagent.cpp qt_gui.cpp
+TRANSLATIONS		= qagent_en.ts
---
+++ qt_gui/qagent_en.ts
@@ -0,0 +1,223 @@
+<!DOCTYPE TS><TS>
+<codec>iso8859-1</codec>
+<context>
+    <name>QAgent</name>
+    <message>
+        <source>QAgent</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>introTextLabel</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Introduction</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>TextLabel1</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>ButtonGroup2</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Standard Path</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>TextLabel2</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Advanced Path</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>TextLabel3</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>TextLabel4</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Select Mode</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Product Selection</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>scanningTextLabel</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>System Detection</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>describeFindingsLabel</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Results</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>userCommentLabel</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>User Comment</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>E-mail Address</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Registration Number (optional)</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Full Name</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>contactInfoLabel</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Contact Information</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>If you would like to send an official bug report via Fenris, please click here. Otherwise, press NEXT.</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Description of Bug</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>To obtain a Fenris login and password, please visit http://fenris.lokigames.com</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>File Bug Report Via Fenris</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Fenris</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Use Encryption (requires GNU Privacy Guard)</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>dispatchMechanismLabel</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Dispatch Mechanism</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Send via e-mail</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Save as Text File</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>dumpFileLabel</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Choose File...</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Dump to File</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>In this version of the QAgent software package, a previously installed copy of GNU Privacy Guard is required in order to encrypt your data.
+
+GNU Privacy Guard is available from http://www.gnupg.org, and is licensed under the GNU General Public License.</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>No GnuPG</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>privacyLabel</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Privacy Policy</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>I&apos;m sure!</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>finalCheckLabel</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Final Check</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>Completion</source>
+        <translation type='unfinished'></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>intro_text</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>scan_text</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>findings_text</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>usercomment_text</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>dispatch_mechanism_text</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>contact_info_text</source>
+        <translation type='unfinished'></translation>
+    </message>
+    <message>
+        <source>final_check_text</source>
+        <translation type='unfinished'></translation>
+    </message>
+</context>
+</TS>
