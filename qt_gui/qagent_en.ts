304314f9d54880e7dc3e2c54150ea491a29a6789
mordred <mordred@928a95fc-701e-0410-818f-83ad171ca9e6>
2001-01-11 20:42:31 +0000

    Stuff.

--- qt_gui/qagent_en.ts
+++ qt_gui/qagent_en.ts
@@ -4,151 +4,151 @@
     <name>QAgent</name>
     <message>
         <source>QAgent</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>QAgent</translation>
     </message>
     <message>
         <source>introTextLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Welcome to the Loki QAgent. This program is designed to gather information about your machine configuration in order to enable our technical support department to assist you further.</translation>
     </message>
     <message>
         <source>Introduction</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Introduction</translation>
     </message>
     <message>
         <source>choosePathLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Please select your mode of operation and press NEXT to continue.</translation>
     </message>
     <message>
         <source>pathButtonGroup</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Modes</translation>
     </message>
     <message>
         <source>Standard Path</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Standard Mode</translation>
     </message>
     <message>
         <source>standardPathLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>For most users.</translation>
     </message>
     <message>
         <source>Advanced Path</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Advanced Mode</translation>
     </message>
     <message>
         <source>advancedPathLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>For experienced users and Loki beta testers only.</translation>
     </message>
     <message>
         <source>pressNextLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>After you have selected an operating mode, please press NEXT to continue.</translation>
     </message>
     <message>
         <source>Select Mode</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Select Mode</translation>
     </message>
     <message>
         <source>chooseProductLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Please choose which product is not working properly, or NONE, then press NEXT to continue.</translation>
     </message>
     <message>
         <source>Product Selection</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Product Selection</translation>
     </message>
     <message>
         <source>scanningTextLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>QAgent is now gathering information about your system. Please be patient - this can take a while.</translation>
     </message>
     <message>
         <source>System Detection</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>System Detection</translation>
     </message>
     <message>
         <source>describeFindingsLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>This is the information that QAgent has gathered about your system. Please check to make sure that you aren&apos;t sending any more information than you feel comfortable with, and then press NEXT to continue.</translation>
     </message>
     <message>
         <source>Results</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>RESULTS</translation>
     </message>
     <message>
         <source>userCommentLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>If you would like to send a comment, please do so here.</translation>
     </message>
     <message>
         <source>User Comment</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>User Comment</translation>
     </message>
     <message>
         <source>emailAddressLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>E-mail Address</translation>
     </message>
     <message>
         <source>regNoLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Registration No. (optional)</translation>
     </message>
     <message>
         <source>fullNameLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Full Name</translation>
     </message>
     <message>
         <source>contactInfoLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>In order to assist you better, we need to know a little bit about you. Please fill out the following labels and press NEXT to continue.</translation>
     </message>
     <message>
         <source>Contact Information</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Contact Information</translation>
     </message>
     <message>
         <source>If you would like to send an official bug report via Fenris, please click here. Otherwise, press NEXT.</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>If you would like to send an official bug report via Fenris, please do so here. Otherwise press NEXT.</translation>
     </message>
     <message>
         <source>fenrisLoginLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Fenris Login</translation>
     </message>
     <message>
         <source>fenrisPasswordLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Fenris Password</translation>
     </message>
     <message>
         <source>fenrisBugTextLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Text of Bug report</translation>
     </message>
     <message>
         <source>fenrisInfoLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>For more information on Fenris, please visit http://fenris.lokigames.com</translation>
     </message>
     <message>
         <source>File Bug Report Via Fenris</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>File Bug Report Via Fenris</translation>
     </message>
     <message>
         <source>Fenris</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Fenris</translation>
     </message>
     <message>
         <source>gpgCheckBox</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Encrypt message contents (requires GnuPG installed on system)</translation>
     </message>
     <message>
         <source>dispatchMechanismLabel</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Dispatch Mechanism</translation>
     </message>
     <message>
         <source>dispatchMechanismGroup</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Dispatch Mechanism</translation>
     </message>
     <message>
         <source>emailDispatchButton</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Send report via e-mail (requires an operational Internet connection)</translation>
     </message>
     <message>
         <source>fileDispatchButton</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Save report to disk</translation>
     </message>
     <message>
         <source>Dispatch Mechanism</source>
-        <translation type='unfinished'></translation>
+        <translation type='unfinished'>Dispatch Mechanism</translation>
     </message>
     <message>
         <source>dumpFileLabel</source>
